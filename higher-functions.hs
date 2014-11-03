2fd5ab642a9edb3dfe93c026b0d68348d2c87f6b
gs-akhan <akhan@gainsight.com>
2014-11-03 13:52:47 +0530

    Adding even filtrs

--- higher-functions.hs
+++ higher-functions.hs
@@ -12,4 +12,29 @@ Function currying example.
 mulBy2 = multiplyBy 2
 mulBy3 = multiplyBy 3
 
+{-
+Example on passing functions as arguments
+-}
+applyTwice :: (a -> a) -> a -> a  
+applyTwice f x = f (f x) 
+
+{-
+Filter a list with only even numbers. that is if the value in 
+-}
+
+isEven :: Int -> Bool
+isEven a =
+	if(a `mod` 2 == 0)
+		then True
+		else False 
+
+
+filterEven :: (Int -> Bool) -> [Int] -> [Int]
+filterEven f xs
+		| otherwise = [a | a <- xs, f a]
+
+
+{-Simplifying the above example-}
+filterEven' :: [Int] -> [Int]
+filterEven' a = [z | z <- a, isEven z]
 
