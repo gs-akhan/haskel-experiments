7879328865d0b798be49fe100c5dc624400f99ca
gs-akhan <akhan@gainsight.com>
2014-11-10 18:30:16 +0530

    Added some more currying

--- currying.hs
+++ currying.hs
@@ -10,3 +10,13 @@ divThree = ( /3 )
 multiplyTheListBy2 = map (*2) [1..20]
 
 divideListBy2 = map (/2) [1..20]
+
+curriedFunctions xs = map (\x y  -> x * y) xs
+
+currFooAtIdx idx = curriedFunctions [1..20] !! idx
+
+curriedFunctionsUsingFoldl xs = foldl (\acc curr -> acc ++ [\a -> curr * a]) [] xs
+
+
+
+
